b7d48c5ee6d92877dc49830f569e03e92990796d
Irina <kis.93@mail.ru>
2025-05-05 19:12:07 +0300

    Реализация GenerateToken

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/aventhis/practice_avito
 go 1.23.1
 
 require github.com/google/uuid v1.6.0
+
+require github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,11 +1,39 @@
 package auth
 
+import (
+	"fmt"
+	jwt "github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
 type AuthService struct {
 	JWTSecret string
+}
+
+type Claims struct {
+	Role string `json:"role"`
+	jwt.RegisteredClaims
 }
 
 func NewAuthService(JWTSecret string) *AuthService {
 	return &AuthService{
 		JWTSecret: JWTSecret,
 	}
+}
+
+func (a *AuthService) GenerateToken(role string) (string, error) {
+	claims := &Claims{
+		Role: role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	singnedToken, err := token.SignedString([]byte(a.JWTSecret))
+	if err != nil {
+		return "", fmt.Errorf("ошибка при генерации токена: %w", err)
+	}
+	return singnedToken, nil
 }
